b7c41f78a9f689872651d2d1208f27336e8b78e5
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2017-01-29 14:23:37 +0100

    Add form.html and property.html

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,4 +2,25 @@ var nyslApp = angular.module('realInvestApp', []);
 
 nyslApp.controller('realInvestController', function($scope){
     $scope.properties = properties;
+
+    $scope.city = {
+        "type": "select", 
+        "name": "city",
+        "value": "Barcelona", 
+        "values": ["Barcelona"] 
+    };
+
+    $scope.income = {
+        "type": "select", 
+        "name": "income",
+        "value": "Selecciona tu capacidad de ahorro", 
+        "values": ["Selecciona tu capacidad de ahorro","Inferior a 800€", "800-1500€", "1500-2500€", "Superior a 2500€"] 
+    };
+
+    $scope.savings = {
+        "type": "select", 
+        "name": "savings",
+        "value": "savings", 
+        "values": ["Selecciona tu capacidad de ahorro","Inferior a 800€", "800-1500€", "1500-2500€", "Superior a 2500€"] 
+    };
 });
